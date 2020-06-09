e72b2cbacc0c5b7cd565d2b8abc6ac9588395a46
yellemonster <block9services@gmail.com>
2020-06-09 16:30:58 -0600

    Further contact functionality and testing

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,16 +1,41 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract Token {
+    using SafeMath for uint256;
+
     string public name;
     string public symbol;
     uint256 public decimals;
     uint256 public totalSupply;
 
+    // Track balances
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor() public {
         name = "Pvblic Token";
         symbol = "PVB";
         decimals = 18;
+
         totalSupply = 1000000 * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_to != address(0), "Address not available");
+        require(balanceOf[msg.sender] >= _value, "Value exceeds balance");
+
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
